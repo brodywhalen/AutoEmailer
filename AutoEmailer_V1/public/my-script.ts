e1b0e15bf3d7a8fea013a339bccc12281bfd7041
brodywhalen <123722295+brodywhalen@users.noreply.github.com>
2025-01-03 02:02:20 -0600

    styled button

--- AutoEmailer_V1/public/my-script.ts
+++ AutoEmailer_V1/public/my-script.ts
@@ -1,3 +1,6 @@
-document.body.style.backgroundColor = "yellow";
+//document.body.style.backgroundColor = "yellow";
 const button = document.createElement("button");
+button.textContent = 'Mass Email';
+button.className = 'Menu-Button' ;
+
 document.body.getElementsByClassName("z0")[0].appendChild(button)
